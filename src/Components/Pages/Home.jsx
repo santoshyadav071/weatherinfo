0f275111c32c7ae86f91ab1052d02c4533b0cb0a
santoshyadav071 <sy7304063887@gmail.com>
2025-05-17 16:14:48 +0530

    spealing correct

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center p-4 rain-effect" style={{ backgroundImage: 'url(/images/rain2.png)' }}>
       <img src="/images/banner.png" alt="Banner" className="animate-bounce w-full max-w-[300px] h-auto rounded object-cover" />
-      <h1 className="text-3xl md:text-4xl font-bold mb-4 animate-bounce text-center">Wecome to the Weather Info App</h1>
+      <h1 className="text-3xl md:text-4xl font-bold mb-4 animate-bounce text-center">Welcome to the Weather Info App</h1>
       <p className="text-base md:text-lg text-center mb-8 animate-bounce">Get the latest weather updates and forecasts for your location.</p>
       <button className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 text-sm md:text-base">
         <Link to="/cheakweather">
